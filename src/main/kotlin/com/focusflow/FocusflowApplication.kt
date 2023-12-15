bd5e8d5e0bd8988f193c39045a5649342782e9f4
Iamwwjd <iamxxjd@gmail.com>
2023-12-15 14:43:43 +0900

    Changes

---
+++ src/main/kotlin/com/focusflow/FocusflowApplication.kt
@@ -0,0 +1,11 @@
+package com.focusflow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FocusflowApplication
+
+fun main(args: Array<String>) {
+	runApplication<FocusflowApplication>(*args)
+}
