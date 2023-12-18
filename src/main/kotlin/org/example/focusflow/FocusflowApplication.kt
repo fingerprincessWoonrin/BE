0d20e63bd103e4520d75704850f95e0af714c998
eggfry313 <106800191+eggfry313@users.noreply.github.com>
2023-12-18 20:14:40 +0900

    Set up initial project structure and add basic dependencies

---
+++ src/main/kotlin/org/example/focusflow/FocusflowApplication.kt
@@ -0,0 +1,11 @@
+package org.example.focusflow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FocusflowApplication
+
+fun main(args: Array<String>) {
+    runApplication<FocusflowApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/focusflow/FocusflowApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.focusflow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FocusflowApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
