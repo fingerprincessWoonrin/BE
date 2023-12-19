bc389c2df215cfb58c656cf9a545c58c9b44edde
eggfry313 <106800191+eggfry313@users.noreply.github.com>
2023-12-19 09:15:19 +0900

    spring version update & create package global, domain

--- src/main/kotlin/org/example/focusflow/global/FocusflowApplication.kt
+++ src/main/kotlin/org/example/focusflow/global/FocusflowApplication.kt
@@ -1,4 +1,4 @@
-package org.example.focusflow
+package org.example.focusflow.global
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- src/main/kotlin/org/example/focusflow/repository/UserRepository.kt
+++
@@ -1,9 +0,0 @@
-package org.example.focusflow.repository
-
-import org.example.focusflow.entity.UserEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepository:JpaRepository<UserEntity, Long> {
-    fun findByUserName(userName:String): UserEntity?
-    fun findByEmail(email:String): UserEntity?
-}
